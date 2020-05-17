fc90ede9fa142cdb9407d101927cc021ad3125ca
Dhruv Garg <48280400+gargdhruv58@users.noreply.github.com>
2020-05-17 19:18:09 +0530

    Start

---
+++ app/src/main/java/com/example/e_placement/AdapterItems.java
@@ -0,0 +1,12 @@
+package com.example.e_placement;
+
+class AdapterItems {
+    public  int ID;
+    public  String product;
+    AdapterItems(int ID, String product)
+    {
+        this.ID=ID;
+        this.product=product;
+
+    }
+}
